5fc93b2c1c8e3b7f149763ac0b4d345b605921df
Jihunp <edwardpark.park@gmail.com>
2023-03-29 12:09:24 -0700

    major frontend changes

--- church-frontend/src/App.js
+++ church-frontend/src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
+import Axios from "axios";
 
 //import components
 import Home from "./pages/Home"
 import About from "./pages/About"
 
 function App() {
+  const [data, setData] = useState("");
+
+  // const getData = async() => {
+  //   const response = await Axios.get("http://localhost:4000/api/ffpc");
+  //   setData(response.data);
+  // }
+
+  const url = 'http://localhost:5000/'
+  const getAllData = () => {
+    Axios.get(`${url}ffpc`)
+    .then((response) => {
+      const allData = response.data.body
+    })
+    .catch(error => console.error`Error:${error}`)
+  }
+
+// next step is to find out how to connect front end and backend together
+  useEffect(() => {
+    getAllData()
+  }, []);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home data={data}/>} />
         <Route path="/about" element={<About/>}/>
       </Routes>
     </div>
--- church-frontend/src/pages/Home.jsx
+++ church-frontend/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-export default function Home() {
+export default function Home(props) {
+
+  console.log(props.data)
   return (
     <>
     <h1> Hello this is the Home page</h1>
