d83d288862595068077f2f8e8c3d8b3cd4c77b6e
Jihunp <edwardpark.park@gmail.com>
2023-04-15 20:56:13 -0700

    styled contact page to have a contact us form

--- church-frontend/src/components/Contact.jsx
+++ church-frontend/src/components/Contact.jsx
@@ -6,10 +6,64 @@ const Section = styled.div`
   scroll-snap-align: center;
 `
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`
+
+const LeftContent = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+
+const RightContent = styled.div`
+  flex: 1;
+`
+
+const Title = styled.h1`
+  font-weight: 200;
+`
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: lightgray;
+`
+
+const TextArea = styled.textarea`
+  padding: 20px;
+`
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`
+
+const Button = styled.button`
+`
 
 const Contact = () => {
   return (
-    <Section> Contact</Section>
+    <Section>
+      <Container>
+        <LeftContent>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" />
+            <Input placeholder="Write your message" />
+            <button>send</button>
+          </Form>
+        </LeftContent>
+      </Container>
+    </Section>
   )
 }
 
