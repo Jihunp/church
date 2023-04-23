a8232e276564ceff14af2213a89b7f93bd0038ba
Jihunp <edwardpark.park@gmail.com>
2023-04-22 20:51:30 -0700

    finished with google maps api

--- church-frontend/src/components/Map.jsx
+++ church-frontend/src/components/Map.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { GoogleMap, useLoadScript, MarkerF, LoadScript, AdvancedMarkerView } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '400px',
-  height: '400px',
+  width: '80vh',
+  height: '80vh',
 }
 
 const center = {
@@ -15,19 +15,6 @@ const position = {
   lat: 47.3426728,
   lng: -122.2909152
 }
-/*
-
-function map
-map.addListener("center_changed", () => {
-  window.setTimeout(() => {
-  }, 3000);
-  });
-  marker.addListener("click", () => {
-    map.setZoon()
-  })
-})
-*/
-
 
 const Map = () => {
   const API_URL = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -37,13 +24,12 @@ const Map = () => {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={16}
+        zoom={15}
         >
           <MarkerF
           position={position}
-          label={"Federal Way First"}
+          label={"FFPC"}
           clickable={true}
-          // onClick={() => setZoom(16)}
           />
       </GoogleMap>
     </LoadScript>
